0ce51101785707295b6b00b99246c8c7738c19e8
Arina-loiko <arina.loikoloiko@yandex.ru>
2026-02-11 15:28:01 +0300

    добавление меню

--- server/Program.cs
+++ server/Program.cs
@@ -2,3 +2,9 @@ Console.WriteLine("Привет!");
 Console.WriteLine("ФИО: Лойко Арина Станиславна");
 Console.WriteLine("Группа: ИСП-232");
 Console.WriteLine("Дата и время: " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+
+Console.WriteLine("\nМеню:");
+Console.WriteLine("1 — Показать ФИО");
+Console.WriteLine("2 — Показать группу");
+Console.WriteLine("3 — Показать дату");
+Console.WriteLine("4 — Выход");
