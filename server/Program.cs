867ecb90f204b97b8763def6160fda9001f24cb9
Arina-loiko <arina.loikoloiko@yandex.ru>
2026-02-11 15:27:53 +0300

    добавление группы и даты

--- server/Program.cs
+++ server/Program.cs
@@ -1,2 +1,4 @@
 Console.WriteLine("Привет!");
 Console.WriteLine("ФИО: Лойко Арина Станиславна");
+Console.WriteLine("Группа: ИСП-232");
+Console.WriteLine("Дата и время: " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
