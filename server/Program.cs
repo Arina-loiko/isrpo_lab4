c52245f03d34b9c729e0dda4bf51213824fd494d
Arina-loiko <arina.loikoloiko@yandex.ru>
2026-02-11 15:28:12 +0300

    добавление логики выбора

--- server/Program.cs
+++ server/Program.cs
@@ -3,8 +3,33 @@ Console.WriteLine("ФИО: Лойко Арина Станиславна");
 Console.WriteLine("Группа: ИСП-232");
 Console.WriteLine("Дата и время: " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
 
-Console.WriteLine("\nМеню:");
-Console.WriteLine("1 — Показать ФИО");
-Console.WriteLine("2 — Показать группу");
-Console.WriteLine("3 — Показать дату");
-Console.WriteLine("4 — Выход");
+while (true)
+{
+    Console.WriteLine("\nМеню:");
+    Console.WriteLine("1 — Показать ФИО");
+    Console.WriteLine("2 — Показать группу");
+    Console.WriteLine("3 — Показать дату");
+    Console.WriteLine("4 — Выход");
+    Console.Write("Ваш выбор: ");
+
+    string choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            Console.WriteLine("Лойко Арина Станиславна");
+            break;
+        case "2":
+            Console.WriteLine("ИСП-232");
+            break;
+        case "3":
+            Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy"));
+            break;
+        case "4":
+            Console.WriteLine("Выход");
+            return;
+        default:
+            Console.WriteLine("Неверный выбор");
+            break;
+    }
+}
